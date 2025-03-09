89020dd273388cfe111953bbeb644d4fac0d6457
faraslutfi <faraslutfi13@gmail.com>
2025-03-10 02:02:57 +0700

    menambahkan implementasi dengan table-driven

--- tpmodul4_1302204106/Program.cs
+++ tpmodul4_1302204106/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+namespace tpmodul4_1302204106
+{
+    class KodePos
+    {
+        private static Dictionary<string, string> kodePosTable = new Dictionary<string, string>
+        {
+            { "Batununggal", "40266" },
+            { "Kujangsari", "40287" },
+            { "Mengger", "40267" },
+            { "Wates", "40256" },
+            { "Cijaura", "40287" },
+            { "Jatisari", "40286" },
+            { "Margasari", "40286" },
+            { "Sekejati", "40286" },
+            { "Kebonwaru", "40272" },
+            { "Maleer", "40274" },
+            { "Samoja", "40273" }
+        };
+
+        public static string GetKodePos(string kelurahan)
+        {
+            return kodePosTable.ContainsKey(kelurahan) ? kodePosTable[kelurahan] : "Kode Pos tidak ditemukan";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Masukkan nama kelurahan: ");
+            string kelurahan = Console.ReadLine();
+            string kodePos = KodePos.GetKodePos(kelurahan);
+            Console.WriteLine($"Kode pos {kelurahan}: {kodePos}");
+        }
+    }
+}
